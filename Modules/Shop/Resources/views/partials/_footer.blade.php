3a4a5ad89e3e4ddec6ffe016ab489af4423b9cec
ugur muslim <ugur.muslim@gmail.com>
2018-09-06 00:54:42 +0300

    Petit front page and header correction

--- Modules/Shop/Resources/views/partials/_footer.blade.php
+++ Modules/Shop/Resources/views/partials/_footer.blade.php
@@ -3,34 +3,9 @@
     <div class="row">
       <div class="col-sm-6 col-lg-3 p-b-50">
         <h4 class="stext-301 cl0 p-b-30">
-          {{__('views.frontend.footer.menu_categories')}}
+          {{__('views.frontend.footer.wnat_to_work_with_us')}}
         </h4>
 
-        <ul>
-          <li class="p-b-10">
-            <a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-             Pantolon
-            </a>
-          </li>
-
-          <li class="p-b-10">
-            <a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-              Ceket
-            </a>
-          </li>
-
-          <li class="p-b-10">
-            <a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-              T-shirt
-            </a>
-          </li>
-
-          <li class="p-b-10">
-            <a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-              Büstiyer
-            </a>
-          </li>
-        </ul>
       </div>
 
 
@@ -39,11 +14,11 @@
           İletişime Geçin
         </h4>
 
-        <p class="stext-107 cl7 size-201">
-          Sorunuz mu var? 0564 987 65 45 numarasından ulaşabilirsiniz.
+        <p class="stext-107 cl7 size-201 ml-3">
+          Sorunuz mu var? 0532 640 56 18 numarasından ulaşabilirsiniz.
         </p>
 
-        <div class="p-t-27">
+        <div class="p-t-27 ml-4">
           <a href="https://www.instagram.com/behicesglm/" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
             <i class="fa fa-instagram"></i>
           </a>
@@ -56,8 +31,8 @@
     <div class="p-t-40">
       <p class="stext-107 cl6 txt-center">
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> Bütün Hakları Gizlidir| Bu site <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank">Sarebu</a> tarafından yapılmıştır.
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> Bütün Hakları Gizlidir| Bu site <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://ugurmuslim.com" target="_blank">Sarebu</a> tarafından yapılmıştır.
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 
       </p>
     </div>
