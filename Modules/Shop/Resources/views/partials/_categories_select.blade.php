e82524f23d50f6a6b5ac39384d7bdd82313cd919
ugur muslim <ugur.muslim@gmail.com>
2018-09-06 12:21:13 +0300

    shop index

--- Modules/Shop/Resources/views/partials/_categories_select.blade.php
+++ Modules/Shop/Resources/views/partials/_categories_select.blade.php
@@ -3,13 +3,13 @@
     Hepsi
   </button>
 
-  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','erkek-t-shirt')->first()->slug}}">
+  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','erkek-t-shirt')->first()->name}}">
     {{$category = $categories->where('slug','erkek-t-shirt')->first()->name}}
   </button>
-  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','unisex-sweat-shirt')->first()->slug}}">
-    {{$category = $categories->where('slug','erkek-t-shirt')->first()->name}}
+  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','unisex-sweat-shirt')->first()->name}}">
+    {{$category = $categories->where('slug','kadın-pantolon')->first()->name}}
   </button>
-  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','kadın-pantolon')->first()->slug}}">
-    {{$category = $categories->where('slug','erkek-t-shirt')->first()->name}}
+  <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$category = $categories->where('slug','kadın-pantolon')->first()->name}}">
+    {{$category = $categories->where('slug','unisex-sweat-shirt')->first()->name}}
   </button>
 </div>
