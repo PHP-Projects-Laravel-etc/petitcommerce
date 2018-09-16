1e58403f64301fef4bb6d00334def94072c2d479
ugur muslim <ugur.muslim@gmail.com>
2018-09-16 13:54:45 +0300

    Product report search href

--- Modules/Cart/Http/Controllers/CheckoutController.php
+++ Modules/Cart/Http/Controllers/CheckoutController.php
@@ -83,8 +83,8 @@ class CheckoutController extends Controller
       $order->productsale->statu = true;
       $order->productsale->save();
     }
-    Mail::to(User::where('email','ugur.muslim@gmail.com')->first())->send(new SendSaleSuccess(19,3));
-    Mail::to(User::where('email','ugur.muslim@gmail.com')->first())->send(new AdminSaleSuccess(19,3));
+    Mail::to(User::where('id',$order[0]->productsale->customer_id)->first())->send(new SendSaleSuccess($orders[0]->sale_package_id,$orders[0]->$adress_id));
+    Mail::to(User::where('email','ugur.muslim@gmail.com')->first())->send(new AdminSaleSuccess($orders[0]->sale_package_id,$orders[0]->$adress_id));
 
 
     ## BURADA YAPILMASI GEREKENLER
