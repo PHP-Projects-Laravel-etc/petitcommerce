416fc72c35154658902e65ea0a527d2ecaff2e44
ugur muslim <ugur.muslim@gmail.com>
2018-09-17 12:40:05 +0300

    Product report search href

--- Modules/Product/Http/Controllers/Frontend/ProductController.php
+++ Modules/Product/Http/Controllers/Frontend/ProductController.php
@@ -34,7 +34,6 @@ class ProductController extends Controller
     $product = Product::where('slug',$slug)->first();
     $product_category = $product->category;
     $related_products = $product_category->products()->where('deleted',false)->take(4)->inRandomOrder()->get();
-    dd($related_products);
     return view('product::frontend.detail')->withProduct($product)
     ->withRelatedproducts($related_products);
   }
