8d4ac8b9a4164b3a6fca6a914565f4b4b9574e8c
ugur muslim <ugur.muslim@gmail.com>
2018-09-22 11:41:56 +0300

    Product report search href

--- Modules/Shop/Resources/views/layouts/master.blade.php
+++ Modules/Shop/Resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
   <!-- Google Tag Manager (noscript)-->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P47RKGT"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-End Google Tag Manager (noscript)
+<!--End Google Tag Manager (noscript)-->
   @yield('content')
 
   @yield('shop_scripts')
