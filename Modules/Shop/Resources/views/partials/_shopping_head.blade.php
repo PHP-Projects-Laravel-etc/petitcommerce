b42e5de7f270e80139de2bc636418eb783ba3eec
ugur muslim <ugur.muslim@gmail.com>
2018-09-27 17:34:41 +0300

    Making frontend fast 1

--- Modules/Shop/Resources/views/partials/_shopping_head.blade.php
+++ Modules/Shop/Resources/views/partials/_shopping_head.blade.php
@@ -5,15 +5,14 @@
 
 {{--	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">--}}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/iconic/css/material-design-iconic-font.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/linearicons-v1.0.0/icon-font.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/fonts/linearicons-v1.0.0/icon-font.min.css')}}">
 <!--===============================================================================================-->
 {{ Html::style(mix('assets/shop/css/shop.css')) }}
-
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/css/util.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/css/main.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/css/util.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('modules/shop/css/main.css')}}">
 <!--===============================================================================================-->
